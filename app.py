b2284f4b58867c677fc142b8a445601c752d13f1
AbhinandanYadav <138775457+abhinandan0y@users.noreply.github.com>
2025-06-19 13:13:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,25 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
-from flask import Flask, render_template, request, url_for, redirect
-
 
 app = Flask(__name__)
-
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 client = MongoClient('localhost', 27017)
+db = client.BSL
+clusters = db.clusters
 
-db = client.test
-Test = db.Test
-
-
-'''
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-'''
-@app.route('/', methods=('GET', 'POST'))
-def index():
- 
-    all-data_Test = Test.find()
-    return render_template('index.html', Test=all-data_Test)
-    
+    all_clusters = list(clusters.find())  # <-- convert cursor to list
+    print(all_clusters)  # <-- Python 3 print
+    return render_template('index.html', clusters=all_clusters)
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
